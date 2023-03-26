e7ad8c5cd897faa0b8c327dbe70611f659681c58
Konstantinos Panayiotou <klpanagi@gmail.com>
2023-03-26 15:00:13 +0300

    Update rm script

--- rm_bot.sh
+++ rm_bot.sh
@@ -17,10 +17,9 @@ if [ "$HBOT_ID" == "" ]; then
 fi
 
 docker container rm hbot-${HBOT_ID}
-docker image rm                 \
+docker volume rm                \
     hbot-${HBOT_ID}-data        \
     hbot-${HBOT_ID}-conf        \
     hbot-${HBOT_ID}-logs        \
     hbot-${HBOT_ID}-scripts     \
     hbot-${HBOT_ID}-pmmscripts  \
-    hbot-${HBOT_ID}-certs       \
