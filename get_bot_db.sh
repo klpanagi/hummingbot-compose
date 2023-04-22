0fea843d5a2ac2eb3a0312f13b3339c3a74803a8
Konstantinos Panayiotou <klpanagi@gmail.com>
2023-04-22 20:11:56 +0300

    Adds outdir argument for get_bot_db script

--- get_bot_db.sh
+++ get_bot_db.sh
@@ -4,14 +4,17 @@ CONTAINER_PREFIX="hbot-"
 
 help()
 {
-    echo "Usage: ./rm_bot.sh [ -n | --id ]
-                             [ -h | --help  ]"
+    echo """
+Usage: ./get_bot_db.sh [ -n | --id ]
+                       [ -o | --outdir ]
+                       [ -h | --help ]
+"""
     exit 2
 }
 
-SHORT=n:,h
-LONG=id:,help
-OPTS=$(getopt -a -n ls_bot.sh --options $SHORT --longoptions $LONG -- "$@")
+SHORT=n:,o:,h
+LONG=id:,output:,help
+OPTS=$(getopt -a -n get_bot_db.sh --options $SHORT --longoptions $LONG -- "$@")
 
 VALID_ARGUMENTS=$# # Returns the count of arguments that are in short or long options
 
@@ -22,6 +25,10 @@ do
   case "$1" in
     -n | --id )
       HBOT_ID="$2"
+      shift 2
+      ;;
+    -o | --outdir )
+      OUTDIR="$2"
       shift 2
       ;;
     -h | --help)
@@ -43,5 +50,10 @@ if [ -z $HBOT_ID ]; then
     exit 1
 fi
 
+if [ -z $OUTDIR ]; then
+    OUTDIR=./hbot-${HBOT_ID}-data
+fi
+
+
 CONTAINER_ID=$(docker run -d -v hbot-${HBOT_ID}-data:/hbot-data busybox true)
-docker cp $CONTAINER_ID:/hbot-data ./hbot-${HBOT_ID}-data
+docker cp ${CONTAINER_ID}:/hbot-data ${OUTDIR}
